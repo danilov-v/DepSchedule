b34a5be3b198899cfeefc824917a1acc0ccd7bb1
gonchar <gonchar@rit.va>
2019-08-01 11:51:17 +0300

    new version api

---
+++ backend/src/main/java/com/varb/schedule/buisness/models/dto/EventBaseReqDto.java
@@ -0,0 +1,161 @@
+package com.varb.schedule.buisness.models.dto;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.*;
+
+/**
+ * EventBaseReqDto
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-08-01T11:31:39.739946+03:00[Europe/Minsk]")
+
+public class EventBaseReqDto   {
+  @JsonProperty("unitId")
+  private Long unitId;
+
+  @JsonProperty("dateFrom")
+  private LocalDate dateFrom;
+
+  @JsonProperty("note")
+  private String note;
+
+  @JsonProperty("eventTypeId")
+  private Long eventTypeId;
+
+  public EventBaseReqDto unitId(Long unitId) {
+    this.unitId = unitId;
+    return this;
+  }
+
+  /**
+   * Id подразделения (может ссылаться только на подразделение 4-го уровня)
+   * @return unitId
+  */
+  @ApiModelProperty(required = true, value = "Id подразделения (может ссылаться только на подразделение 4-го уровня)")
+  @NotNull
+
+
+  public Long getUnitId() {
+    return unitId;
+  }
+
+  public void setUnitId(Long unitId) {
+    this.unitId = unitId;
+  }
+
+  public EventBaseReqDto dateFrom(LocalDate dateFrom) {
+    this.dateFrom = dateFrom;
+    return this;
+  }
+
+  /**
+   * Дата начала события
+   * @return dateFrom
+  */
+  @ApiModelProperty(required = true, value = "Дата начала события")
+  @NotNull
+
+  @Valid
+
+  public LocalDate getDateFrom() {
+    return dateFrom;
+  }
+
+  public void setDateFrom(LocalDate dateFrom) {
+    this.dateFrom = dateFrom;
+  }
+
+  public EventBaseReqDto note(String note) {
+    this.note = note;
+    return this;
+  }
+
+  /**
+   * Примечание
+   * @return note
+  */
+  @ApiModelProperty(value = "Примечание")
+
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+  public EventBaseReqDto eventTypeId(Long eventTypeId) {
+    this.eventTypeId = eventTypeId;
+    return this;
+  }
+
+  /**
+   * Ссылка на тип события 
+   * @return eventTypeId
+  */
+  @ApiModelProperty(required = true, value = "Ссылка на тип события ")
+  @NotNull
+
+
+  public Long getEventTypeId() {
+    return eventTypeId;
+  }
+
+  public void setEventTypeId(Long eventTypeId) {
+    this.eventTypeId = eventTypeId;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    EventBaseReqDto eventBaseReq = (EventBaseReqDto) o;
+    return Objects.equals(this.unitId, eventBaseReq.unitId) &&
+        Objects.equals(this.dateFrom, eventBaseReq.dateFrom) &&
+        Objects.equals(this.note, eventBaseReq.note) &&
+        Objects.equals(this.eventTypeId, eventBaseReq.eventTypeId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(unitId, dateFrom, note, eventTypeId);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class EventBaseReqDto {\n");
+    
+    sb.append("    unitId: ").append(toIndentedString(unitId)).append("\n");
+    sb.append("    dateFrom: ").append(toIndentedString(dateFrom)).append("\n");
+    sb.append("    note: ").append(toIndentedString(note)).append("\n");
+    sb.append("    eventTypeId: ").append(toIndentedString(eventTypeId)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ backend/src/main/java/com/varb/schedule/buisness/models/dto/EventTypePostDto.java
@@ -0,0 +1,108 @@
+package com.varb.schedule.buisness.models.dto;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.*;
+
+/**
+ * EventTypePostDto
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-08-01T11:31:39.739946+03:00[Europe/Minsk]")
+
+public class EventTypePostDto   {
+  @JsonProperty("description")
+  private String description;
+
+  @JsonProperty("color")
+  private String color;
+
+  public EventTypePostDto description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * Описание события
+   * @return description
+  */
+  @ApiModelProperty(required = true, value = "Описание события")
+  @NotNull
+
+@Size(min=3) 
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public EventTypePostDto color(String color) {
+    this.color = color;
+    return this;
+  }
+
+  /**
+   * Отображаемый цвет
+   * @return color
+  */
+  @ApiModelProperty(required = true, value = "Отображаемый цвет")
+  @NotNull
+
+@Size(min=3) 
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    EventTypePostDto eventTypePost = (EventTypePostDto) o;
+    return Objects.equals(this.description, eventTypePost.description) &&
+        Objects.equals(this.color, eventTypePost.color);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(description, color);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class EventTypePostDto {\n");
+    
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("    color: ").append(toIndentedString(color)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
