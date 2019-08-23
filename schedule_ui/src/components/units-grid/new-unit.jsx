7b85ea8cd8cf056357e841fb4d94a621f9ce8e5f
Илья Волосюк <volosyuk@RIT-L006.rit.va>
2019-08-23 10:52:01 +0300

    update cursor

--- schedule_ui/src/components/units-grid/new-unit.jsx
+++ schedule_ui/src/components/units-grid/new-unit.jsx
@@ -7,6 +7,7 @@ export function NewUnit({ onAddUnit }) {
     <div
       className={classnames(
         "unit",
+        "unit-new",
         "d-flex",
         "justify-content-center",
         "align-items-center",
