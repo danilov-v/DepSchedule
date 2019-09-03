cfa74e3fb9aae9404f20e178be5c456804a7fbc7
ignatovich <none>
2019-09-03 13:01:50 +0300

    Unit test updated

--- backend/src/main/java/com/varb/schedule/config/DataSourceConfiguration.java
+++ backend/src/main/java/com/varb/schedule/config/DataSourceConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
@@ -16,6 +17,7 @@ import java.io.FileNotFoundException;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
+@Profile("!test")
 public class DataSourceConfiguration {
     private static final String DB_URL_PROP_NAME = "spring.datasource.jdbc-url";
     private final ConfigurableApplicationContext applicationContext;
