9a4e5ef48a2e16ae61bd5c484ddf133093cf7cd5
ignatovich <none>
2019-09-13 09:01:41 +0300

    Periods intersection and dates validation added Issue completed - https://git.rit/DepSchedule/DepSchedule/issues/42

--- backend/src/main/java/com/varb/schedule/buisness/logic/repository/PeriodRepository.java
+++ backend/src/main/java/com/varb/schedule/buisness/logic/repository/PeriodRepository.java
@@ -4,6 +4,7 @@ import com.varb.schedule.buisness.models.entity.Period;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PeriodRepository extends JpaRepository<Period, Long> {
@@ -11,4 +12,9 @@ public interface PeriodRepository extends JpaRepository<Period, Long> {
     //Not used now
     @Query("Select p from Period p where p.name = :name")
     List<Period> findByName(String name);
+
+    //don't use equals (<= or >=) in comparison because
+    //the case when a period starts from the date another one finishes is valid
+    @Query("Select p from Period p where :startDate < p.endDate and :endDate > p.startDate")
+    List<Period> findIntersections(LocalDate startDate, LocalDate endDate);
 }
--- backend/src/main/java/com/varb/schedule/buisness/logic/service/PeriodService.java
+++ backend/src/main/java/com/varb/schedule/buisness/logic/service/PeriodService.java
@@ -5,10 +5,14 @@ import com.varb.schedule.buisness.models.dto.PeriodPostDto;
 import com.varb.schedule.buisness.models.dto.PeriodPutDto;
 import com.varb.schedule.buisness.models.entity.Period;
 import com.varb.schedule.config.modelmapper.ModelMapperCustomize;
+import com.varb.schedule.exception.ServiceException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -18,10 +22,32 @@ public class PeriodService extends AbstractService<Period, Long> {
 
     private final PeriodRepository periodRepository;
     private final ModelMapperCustomize modelMapper;
+    private final String DATES_INTERSECTION = "DATES_INTERSECTION";
+    private final String DATES_INTERSECTION_MESSAGE = "Данный период пересекается с уже существующим. Проверьте даты его начала и окончания.";
+
+    private final String WRONG_DATES = "WRONG_DATES";
+    private final String WRONG_DATES_MESSAGE = "Дата начала периода должна предшествовать его окончанию.";
 
     public Period add(PeriodPostDto periodPost) {
-        Period period = modelMapper.map(periodPost, Period.class);
-        return periodRepository.save(period);
+        if (periodPost.getStartDate().isBefore(periodPost.getEndDate())) {
+            List<Period> intersections = periodRepository.
+                    findIntersections(periodPost.getStartDate(), periodPost.getEndDate());
+            if(!intersections.isEmpty()) {
+                String ids = intersections.stream().map(period -> period.getPeriodId().toString()).collect(Collectors.joining(","));
+                throw new ServiceException(
+                        "Period you want to add has intersections with other periods with ids: " +ids,
+                        DATES_INTERSECTION_MESSAGE,
+                        DATES_INTERSECTION);
+            }
+
+        } else {
+            throw new ServiceException(
+                    "startDate " +periodPost.getStartDate() +" >= " +"endDate " +periodPost.getEndDate(),
+                    WRONG_DATES_MESSAGE,
+                    WRONG_DATES);
+        }
+        //if everything is ok - there's no intersections and dates are correct, process it further
+        return periodRepository.save(modelMapper.map(periodPost, Period.class));
     }
 
     public Period update(Long periodId, PeriodPutDto periodPut) {
