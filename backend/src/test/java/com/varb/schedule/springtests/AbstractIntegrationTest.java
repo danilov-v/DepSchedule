9ab39599fa1764b7cc4621b3aef6328800a5b4dc
ignatovich <none>
2019-09-21 11:48:58 +0300

    Add post EventType test - need improvement

--- backend/src/test/java/com/varb/schedule/springtests/AbstractIntegrationTest.java
+++ backend/src/test/java/com/varb/schedule/springtests/AbstractIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.varb.schedule.springtests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +11,17 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class AbstractIntegrationTest {
+public abstract class AbstractIntegrationTest {
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected String asJsonString(Object obj) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(obj);
+    }
 
     //protected final String appJsonMediaType = "application/json;charset=UTF-8";
     //protected final String appJsonContentType = "application/json;charset=UTF-8";
