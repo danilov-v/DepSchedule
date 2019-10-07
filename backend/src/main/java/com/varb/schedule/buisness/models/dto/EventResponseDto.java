b8dab2864cff6b41527ff804f53e6a798f1d58c7
gonchar <gonchar@rit.va>
2019-10-07 16:59:22 +0300

    api fix

--- backend/src/main/java/com/varb/schedule/buisness/models/dto/EventResponseDto.java
+++ backend/src/main/java/com/varb/schedule/buisness/models/dto/EventResponseDto.java
@@ -3,19 +3,39 @@ package com.varb.schedule.buisness.models.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.Objects;
 
 /**
  * EventResponseDto
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-10-07T16:47:13.207234+03:00[Europe/Minsk]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-10-07T16:58:36.192881+03:00[Europe/Minsk]")
 
 public class EventResponseDto   {
   @JsonProperty("duration")
   private Integer duration;
+
+  @JsonProperty("unitId")
+  private Long unitId;
+
+  @JsonProperty("dateFrom")
+  private LocalDate dateFrom;
+
+  @JsonProperty("location")
+  private LocationDto location = null;
+
+  @JsonProperty("planned")
+  private Boolean planned;
+
+  @JsonProperty("note")
+  private String note;
+
+  @JsonProperty("eventTypeId")
+  private Long eventTypeId;
 
   @JsonProperty("eventId")
   private Long eventId;
@@ -41,6 +61,128 @@ public class EventResponseDto   {
 
   public void setDuration(Integer duration) {
     this.duration = duration;
+  }
+
+  public EventResponseDto unitId(Long unitId) {
+    this.unitId = unitId;
+    return this;
+  }
+
+  /**
+   * Id подразделения (может ссылаться только на подразделение 4-го уровня)
+   * @return unitId
+  */
+  @ApiModelProperty(value = "Id подразделения (может ссылаться только на подразделение 4-го уровня)")
+
+
+  public Long getUnitId() {
+    return unitId;
+  }
+
+  public void setUnitId(Long unitId) {
+    this.unitId = unitId;
+  }
+
+  public EventResponseDto dateFrom(LocalDate dateFrom) {
+    this.dateFrom = dateFrom;
+    return this;
+  }
+
+  /**
+   * Дата начала события
+   * @return dateFrom
+  */
+  @ApiModelProperty(value = "Дата начала события")
+
+  @Valid
+
+  public LocalDate getDateFrom() {
+    return dateFrom;
+  }
+
+  public void setDateFrom(LocalDate dateFrom) {
+    this.dateFrom = dateFrom;
+  }
+
+  public EventResponseDto location(LocationDto location) {
+    this.location = location;
+    return this;
+  }
+
+  /**
+   * Get location
+   * @return location
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public LocationDto getLocation() {
+    return location;
+  }
+
+  public void setLocation(LocationDto location) {
+    this.location = location;
+  }
+
+  public EventResponseDto planned(Boolean planned) {
+    this.planned = planned;
+    return this;
+  }
+
+  /**
+   * Запланированность события
+   * @return planned
+  */
+  @ApiModelProperty(value = "Запланированность события")
+
+
+  public Boolean getPlanned() {
+    return planned;
+  }
+
+  public void setPlanned(Boolean planned) {
+    this.planned = planned;
+  }
+
+  public EventResponseDto note(String note) {
+    this.note = note;
+    return this;
+  }
+
+  /**
+   * Примечание
+   * @return note
+  */
+  @ApiModelProperty(value = "Примечание")
+
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+  public EventResponseDto eventTypeId(Long eventTypeId) {
+    this.eventTypeId = eventTypeId;
+    return this;
+  }
+
+  /**
+   * Ссылка на тип события 
+   * @return eventTypeId
+  */
+  @ApiModelProperty(value = "Ссылка на тип события ")
+
+
+  public Long getEventTypeId() {
+    return eventTypeId;
+  }
+
+  public void setEventTypeId(Long eventTypeId) {
+    this.eventTypeId = eventTypeId;
   }
 
   public EventResponseDto eventId(Long eventId) {
@@ -74,12 +216,18 @@ public class EventResponseDto   {
     }
     EventResponseDto eventResponse = (EventResponseDto) o;
     return Objects.equals(this.duration, eventResponse.duration) &&
+        Objects.equals(this.unitId, eventResponse.unitId) &&
+        Objects.equals(this.dateFrom, eventResponse.dateFrom) &&
+        Objects.equals(this.location, eventResponse.location) &&
+        Objects.equals(this.planned, eventResponse.planned) &&
+        Objects.equals(this.note, eventResponse.note) &&
+        Objects.equals(this.eventTypeId, eventResponse.eventTypeId) &&
         Objects.equals(this.eventId, eventResponse.eventId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(duration, eventId);
+    return Objects.hash(duration, unitId, dateFrom, location, planned, note, eventTypeId, eventId);
   }
 
   @Override
@@ -88,6 +236,12 @@ public class EventResponseDto   {
     sb.append("class EventResponseDto {\n");
     
     sb.append("    duration: ").append(toIndentedString(duration)).append("\n");
+    sb.append("    unitId: ").append(toIndentedString(unitId)).append("\n");
+    sb.append("    dateFrom: ").append(toIndentedString(dateFrom)).append("\n");
+    sb.append("    location: ").append(toIndentedString(location)).append("\n");
+    sb.append("    planned: ").append(toIndentedString(planned)).append("\n");
+    sb.append("    note: ").append(toIndentedString(note)).append("\n");
+    sb.append("    eventTypeId: ").append(toIndentedString(eventTypeId)).append("\n");
     sb.append("    eventId: ").append(toIndentedString(eventId)).append("\n");
     sb.append("}");
     return sb.toString();
