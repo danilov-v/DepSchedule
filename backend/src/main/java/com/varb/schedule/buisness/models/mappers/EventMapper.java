49919b13f7b6ee1f6d38e6ea654b78e009817601
gonchar <gonchar@rit.va>
2019-10-11 08:38:17 +0300

    EventMapper fix

--- backend/src/main/java/com/varb/schedule/buisness/models/mappers/EventMapper.java
+++ backend/src/main/java/com/varb/schedule/buisness/models/mappers/EventMapper.java
@@ -1,6 +1,5 @@
 package com.varb.schedule.buisness.models.mappers;
 
-import com.varb.schedule.buisness.models.business.LocationTypeEnum;
 import com.varb.schedule.buisness.models.dto.EventPostDto;
 import com.varb.schedule.buisness.models.dto.EventPutDto;
 import com.varb.schedule.buisness.models.dto.EventResponseDto;
@@ -24,45 +23,23 @@ public class EventMapper {
         AbstractConverter<Event, LocationDto> fillLocationDto = new AbstractConverter<>() {
             @Override
             protected LocationDto convert(Event source) {
-                LocationDto locationDto = new LocationDto();
-                return locationDto.name(source.getLocationName())
-                .type(LocationDto.TypeEnum.fromValue(source.getLocationType().toString()));
-            }
-        };
-
-        AbstractConverter<LocationDto, String> setEventLocationName = new AbstractConverter<>() {
-            @Override
-            protected String convert(LocationDto source) {
-                return source.getName();
-            }
-        };
-
-        AbstractConverter<LocationDto, LocationTypeEnum> setEventLocationType = new AbstractConverter<>() {
-            @Override
-            protected LocationTypeEnum convert(LocationDto source) {
-                return LocationTypeEnum.fromValue(source.getType().toString());
+                return new LocationDto()
+                        .name(source.getLocationName())
+                        .type(LocationDto.TypeEnum.fromValue(source.getLocationType().toString()));
             }
         };
 
         modelMapper.typeMap(Event.class, EventResponseDto.class)
                 .addMappings(mapper -> mapper
-                .using(fillLocationDto)
-                .map(source -> source, EventResponseDto::setLocation));
+                        .using(fillLocationDto)
+                        .map(source -> source, EventResponseDto::setLocation));
 
         modelMapper.typeMap(EventPostDto.class, Event.class)
-                .addMappings(mapper -> mapper
-                        .using(setEventLocationName)
-                        .map(EventPostDto::getLocation, Event::setLocationName))
-                .addMappings(mapper -> mapper
-                        .using(setEventLocationType)
-                        .map(EventPostDto::getLocation, Event::setLocationType));
+                .addMapping(source -> source.getLocation().getName(), Event::setLocationName)
+                .addMapping(source -> source.getLocation().getType(), Event::setLocationType);
 
         modelMapper.typeMap(EventPutDto.class, Event.class)
-                .addMappings(mapper -> mapper
-                        .using(setEventLocationName)
-                        .map(EventPutDto::getLocation, Event::setLocationName))
-                .addMappings(mapper -> mapper
-                        .using(setEventLocationType)
-                        .map(EventPutDto::getLocation, Event::setLocationType));
+                .addMapping(source -> source.getLocation().getName(), Event::setLocationName)
+                .addMapping(source -> source.getLocation().getType(), Event::setLocationType);
     }
 }
