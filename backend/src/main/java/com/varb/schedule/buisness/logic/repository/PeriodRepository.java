48d26364fd7786a90f175549ef44c4512a338cfb
gonchar <gonchar@rit.va>
2019-11-13 12:09:10 +0300

    fix: parentId in PeriodRepository::findIntersections may be null

--- backend/src/main/java/com/varb/schedule/buisness/logic/repository/PeriodRepository.java
+++ backend/src/main/java/com/varb/schedule/buisness/logic/repository/PeriodRepository.java
@@ -3,6 +3,7 @@ package com.varb.schedule.buisness.logic.repository;
 import com.varb.schedule.buisness.models.entity.Period;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.Nullable;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,7 +17,7 @@ public interface PeriodRepository extends JpaRepository<Period, Long> {
     //don't use equals (<= or >=) in comparison because
     //the case when a period starts from the date another one finishes is valid
     @Query("Select p from Period p where p.calendarId = :calendarId and p.parentId = :parentId and :startDate < p.endDate and :endDate > p.startDate")
-    List<Period> findIntersections(Long calendarId, Long parentId, LocalDate startDate, LocalDate endDate);
+    List<Period> findIntersections(Long calendarId, @Nullable Long parentId, LocalDate startDate, LocalDate endDate);
 
     @Query("Select p from Period p where p.calendarId = :calendarId")
     List<Period> findAllByCalendarId(Long calendarId);
